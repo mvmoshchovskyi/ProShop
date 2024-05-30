b60b9bb0e15d4aff0ad412ad248cdb9f5711462e
mmoshchovskyi <mykhailo.m@vivint.com>
2024-05-30 14:30:01 +0300

    Order routes and controller functions

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
+import orderRoutes from "./routes/orderRoutes.js";
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
@@ -21,6 +22,7 @@ app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
