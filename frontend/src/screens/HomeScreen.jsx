37e302e14e93060c043d6c53ccf270d9aef19ce4
mmoshchovskyi <mykhailo.m@vivint.com>
2024-05-31 18:08:18 +0300

    Product carousel

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,10 +1,11 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import {Row, Col} from 'react-bootstrap';
 import Product from '../components/Product';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import Paginate from '../components/Paginate';
+import ProductCarousel from '../components/ProductCarousel';
 
 const HomeScreen = () => {
     const { pageNumber, keyword } = useParams();
@@ -16,6 +17,13 @@ const HomeScreen = () => {
 
     return (
         <>
+            {!keyword ? (
+                <ProductCarousel />
+            ) : (
+                <Link to='/' className='btn btn-light'>
+                    Go Back
+                </Link>
+            )}
             {
                 isLoading ? (
                     <Loader />
