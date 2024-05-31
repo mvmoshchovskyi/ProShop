356d4b478e0db1db445d884a750cc1d3581e0653
mmoshchovskyi <mykhailo.m@vivint.com>
2024-05-31 17:49:19 +0300

    Product pagination & Paginate component

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -4,11 +4,12 @@ import { apiSlice } from './apiSlice';
 export const productsApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getProducts: builder.query({
-            query: () => ({
+            query: ({ pageNumber }) => ({
                 url: PRODUCTS_URL,
+                params: { pageNumber },
             }),
             keepUnusedDataFor: 5,
-            providesTags: ['Products'],
+            providesTags: ['Product'],
         }),
         getProductDetails: builder.query({
             query: (productId) => ({
